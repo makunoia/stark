9258c06d9fcfb6360ba335ebd001f94db246bcc9
marknoya-eventful <marknoya@eventful.ph>
2023-04-21 09:19:28 +0800

    added remixicon peerDependency

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import "remixicon/fonts/remixicon.css";
 
 export { default as Button } from "./Components/Button";
 export { default as InputField } from "./Components/InputField";
