80f9ab39a33eec0649a5adc6bf73491ed046c47c
makunoia <markbriannoya@gmail.com>
2024-05-29 14:51:29 -0400

    Relocated github folder to root

--- apps/sandbox/src/app/page.tsx
+++ apps/sandbox/src/app/page.tsx
@@ -1,13 +1,19 @@
 "use client";
-import { Button } from "@makunoia/stark";
+import { Button, Input, Stack, Text, TextButton } from "@makunoia/stark";
 export default function Home() {
   return (
     <>
-      <Button
-        label="Button"
-        intent="primary"
-        onClick={() => alert("it's working")}
-      />
+      <Stack>
+        <Text variant="title">Test</Text>
+        <Button
+          label="Button"
+          intent="primary"
+          onClick={() => alert("it's working")}
+        />
+        <Text variant="title">test</Text>
+        <TextButton label="Text Button" />
+        <Input />
+      </Stack>
       <div className="bg-inverse text-oninverse">Test</div>
     </>
   );
